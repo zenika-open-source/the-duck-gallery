94f9fa8c4a09362eeed8099f57eb752c43ba4928
Michael Guerrero <maikurusama@gmail.com>
2020-09-24 18:27:43 -0400

    Adding My Duck!

--- contributors.js
+++ contributors.js
@@ -10,4 +10,5 @@
  */
 var contributors = [
   { username: 'bpetetot', message: 'Happy Hacktoberfest 2020!' },
+  { username : 'MikeLikesCode', message: 'I love my duck so much! Happy Hacktoberfest!'}
 ]
