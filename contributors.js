212150303a1f14fdb7755aa9fc89038e2b39c2bf
nagahshi <willian.empari@gmail.com>
2020-09-24 17:36:26 -0300

    feat: new amazing duck

--- contributors.js
+++ contributors.js
@@ -1,13 +1,14 @@
 /**
  * Add here your GitHub username and message.
- * 
+ *
  * Add it at the end, after the last contributor.
- * 
+ *
  * Format:
- * { username: 'your-username', message: 'Say something kind or fun' }, 
+ * { username: 'your-username', message: 'Say something kind or fun' },
  *
  * The message must be short and respect our code of conduct.
  */
 var contributors = [
   { username: 'bpetetot', message: 'Happy Hacktoberfest 2020!' },
+  { username: 'nagahshi', message: 'QUACK! #hacktoberfest' },
 ]
