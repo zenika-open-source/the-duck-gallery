82e6bdd87e574c316a8ea627fc020cb4572b1b1e
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2020-09-25 14:46:31 +0500

    Huzaifaahmed20 - added my duck

--- contributors.js
+++ contributors.js
@@ -1,13 +1,17 @@
 /**
  * Add here your GitHub username and message.
- * 
+ *
  * Add it at the end, after the last contributor.
- * 
+ *
  * Format:
- * { username: 'your-username', message: 'Say something kind or fun' }, 
+ * { username: 'your-username', message: 'Say something kind or fun' },
  *
  * The message must be short and respect our code of conduct.
  */
 var contributors = [
   { username: 'bpetetot', message: 'Happy Hacktoberfest 2020!' },
-]
+  {
+    username: 'Huzaifaahmed20',
+    message: 'I have added my cool duck on this repo.',
+  },
+];
