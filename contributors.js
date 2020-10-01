877354f6dcd832fe4c2555ba75b46098decd2955
anishghimire603 <aghimire603@gmail.com>
2020-10-01 09:16:01 +0545

    Added anishghimire603 Duck!

--- contributors.js
+++ contributors.js
@@ -10,4 +10,5 @@
  */
 var contributors = [
   { username: 'bpetetot', message: 'Happy Hacktoberfest 2020!' },
+  { username: 'anishghimire603', message: 'Have fun and contribute to Open Sources.' },
 ]
