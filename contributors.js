0e34a1ae5633acb35a9e1a90defa36784686b267
Nikhil Omkar <61050785+nikhilomkar99@users.noreply.github.com>
2020-10-01 10:41:21 +0530

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -10,4 +10,5 @@
  */
 var contributors = [
   { username: 'bpetetot', message: 'Happy Hacktoberfest 2020!' },
+  { username: 'nikhilomkar99', message: 'Added a duck.Happy Hacktoberfest 2020!' },
 ]
