2fed01117597499f5b8c544556bb609898af22d4
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2020-10-01 05:18:31 -0400

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -10,6 +10,7 @@
  */
 var contributors = [
   { username: 'bpetetot', message: 'Happy Hacktoberfest 2020!' },
+  { username : 'MikeLikesCode', message: 'I love my duck so much! Happy Hacktoberfest!'},
   { username: 'Ranjeeta01', message: 'Stay calm and code!' },
   { username: 'kchatr', message: 'Here\'s to my first ever Hacktoberfest contribution :D'},
   { username: 'AnggaR96s', message: 'Enjoy #Hacktoberfest2020!' },
