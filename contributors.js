44951a8b6ebfa9b3ad34d81901bd3b86c71d5177
Jean-Phi Baconnais <jeanphilippe.baconnais@gmail.com>
2020-10-01 22:10:39 +0200

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -75,9 +75,10 @@ var contributors = [
   },
   { username: 'Saral33', message: 'Keep coding :)' },
   { username: 'weiyi-m', message: 'happy hacktoberfest!' },
+  { username: 'JoseRivera12', message: 'Happy coding ;)'},
+  { username: 'jeanphibaconnais', message: 'Enjoy Hacktoberfest2020 ! 🤘'},
   { username: 'jlandure', message: '🎉 Happy Hacking 2020! 💚' },
   { username: 'JoseRivera12', message: 'Happy coding ;)'},
   { username: 'Tazminia', message: 'Free PR for everyone'},
   { username: 'chocho01', message: '❤️🦆 & ❤️ Hacktoberfest' },
-  { username: 'JoseRivera12', message: 'Happy coding ;)'},
 ]
