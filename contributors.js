8d6919883b5023b43a9ceb39fb8097a5b0d467d5
Renan Miranda <renanlmiranda@gmail.com>
2020-10-01 06:22:15 -0300

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -11,8 +11,9 @@
 var contributors = [
   { username: 'David-Lazaro-Fernandez', message: 'Ducky DucktoberFest 2020!!' },
   { username: 'bpetetot', message: 'Happy Hacktoberfest 2020!' },
+  { username: 'renanlmiranda', message: 'There is no advantage to living life on the run.' },
   { username: 'ukt1997', message: 'My 1st Contribution to Open Source , Happy Hacktoberfest 2020!' },
-  { username : 'MikeLikesCode', message: 'I love my duck so much! Happy Hacktoberfest!'},
+  { username: 'MikeLikesCode', message: 'I love my duck so much! Happy Hacktoberfest!'},
   { username: 'Ranjeeta01', message: 'Stay calm and code!' },
   { username: 'kchatr', message: 'Here\'s to my first ever Hacktoberfest contribution :D'},
   { username: 'AnggaR96s', message: 'Enjoy #Hacktoberfest2020!' },
