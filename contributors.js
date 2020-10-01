c7fabdf90f0434c7a66a17c958caaf23b13cfa39
Thanawat Gulati <thanawatgulatijames@gmail.com>
2020-10-01 16:56:38 +0700

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -12,6 +12,7 @@ var contributors = [
   { username: 'Kazeno21', message: 'Happy Hacktoberfest 2020:)' },
   { username: 'David-Lazaro-Fernandez', message: 'Ducky DucktoberFest 2020!!' },
   { username: 'bpetetot', message: 'Happy Hacktoberfest 2020!' },
+  { username: 't6tg', message: 'Happy Hacktoberfest 2020! 🐣' },
   { username: 'lvmenezes', message: 'Show must code on' },
   { username: 'paddlepop25', message: 'This is my first PR! Wheeee!' },
   { username: 'Natasha1305', message: 'This was a great first experience. Thank You!' },
