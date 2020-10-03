f6e4eac9477b8a582b2452ce81f44d291cf4826a
Adrien Nortain <adrien.nortain@gmail.com>
2020-10-03 18:05:32 +0800

    Add duck

--- contributors.js
+++ contributors.js
@@ -90,4 +90,5 @@ var contributors = [
   { username: 'jbleduigou', message: 'Hello, Gophers!' },
   { username: "Fabianolvs", message: "Get away from js kids! Just kiding! XD" },
   { username: "werikgpaula", message: "🦆 Bee free" },
+  { username: 'LamSonFei', message: 'For the Hacktoberfest: 🦆 and move... 🦆 and move!' },
 ];
