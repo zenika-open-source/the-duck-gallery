30ec3ad70758f2bc7b1a0b92619e46643321be50
Kwame Violet Taylor <68249329+KwameTaylor@users.noreply.github.com>
2020-10-02 21:52:42 -0500

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -90,4 +90,5 @@ var contributors = [
   { username: 'jbleduigou', message: 'Hello, Gophers!' },
   { username: "Fabianolvs", message: "Get away from js kids! Just kiding! XD" },
   { username: "werikgpaula", message: "🦆 Bee free" },
+  { username: 'KwameTaylor', message: '#BlackLivesMatter #Hacktoberfest2020 :)' },
 ];
