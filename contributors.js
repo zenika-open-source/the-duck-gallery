e2be62981d4a0dfd160826b93d9b5c5b2dc419f0
Marcelino Derry Utomo <30742802+Marcelinoderry@users.noreply.github.com>
2020-10-04 19:21:27 +0800

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -92,6 +92,7 @@ var contributors = [
   { username: 'jbleduigou', message: 'Hello, Gophers!' },
   { username: "Fabianolvs", message: "Get away from js kids! Just kiding! XD" },
   { username: "werikgpaula", message: "🦆 Bee free" },
+  { username: 'Marcelinoderry', message: 'Happy Hacktoberfest 2020 #popo[L]' },
   { username: 'LNCH', message: 'Darth Ducky shall take over the world!' },
   { username: "mateahoward", message: "Happy Coding 💚" },
   { username: 'cleisoncarlos', message: '🦆 Happy Quacktoberfest 2020!' },
