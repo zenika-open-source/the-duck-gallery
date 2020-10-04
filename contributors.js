c9523132673fd6d15ff920c038f81d7a14f9f1b9
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-10-04 13:34:01 +0200

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -94,6 +94,7 @@ var contributors = [
   { username: 'jbleduigou', message: 'Hello, Gophers!' },
   { username: "Fabianolvs", message: "Get away from js kids! Just kiding! XD" },
   { username: "werikgpaula", message: "🦆 Bee free" },
+  { username: "yannbertrand", message: "Bonjour monde ! Merci ❤️" },
   { username: 'LamSonFei', message: 'For the Hacktoberfest: 🦆 and move... 🦆 and move!' },
   { username: 'karinesabatier', message: 'Happy Hacking to everyone!' },
   { username: "Cajs", message: "🦆 #Hacktoberfest2020 🦆" },
