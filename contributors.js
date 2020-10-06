a8fc1c46126f96f91b39346c56c197003801a9dc
Rémi Briois <remi.briois@zenika.com>
2020-10-06 20:51:14 +0200

    Add my duck so one day ducks will rule the world

--- contributors.js
+++ contributors.js
@@ -116,4 +116,5 @@ var contributors = [
   { username: 'axelrammon', message: 'Happy Hacktoberfest 2020 and drink coffee!!' },
   { username: 'Tejasvi2', message: 'Happy Coding!!! #Hacktoberfest2020' },
   { username: 'ElekN', message: 'Happy Hacktoberfest :DDDDDD' },
+  { username: 'rbriois', message: 'Happy Hacktoberfest !! Ducks rulez Da World !!' },
 ];
