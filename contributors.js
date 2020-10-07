b8340177b6b4bc85a5ad52dc16d2df6ddf5293b1
David Da Silva <contact@daviddasilva.net>
2020-10-07 13:53:19 +0200

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -117,6 +117,7 @@ var contributors = [
   { username: 'axelrammon', message: 'Happy Hacktoberfest 2020 and drink coffee!!' },
   { username: 'Tejasvi2', message: 'Happy Coding!!! #Hacktoberfest2020' },
   { username: 'ElekN', message: 'Happy Hacktoberfest :DDDDDD' },
+  { username: 'david-dasilva', message: 'MacGyver approves this Hacktoberfest!' },
   { username: 'julialehoux', message: 'Hacktoberfest c’est la vie' },
   { username: 'nbrdx', message: 'I wish you a happy ZenikHacktoberfest2020 !' },
   { username: 'Aurore-Husson', message: 'Joyeux Hacktoberfest à tous :)' },
