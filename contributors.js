6cf0e1df832538c97db300296ee77eabab7608de
Dhruvin Balar <35475783+drbalar@users.noreply.github.com>
2020-10-07 07:42:10 -0400

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -116,6 +116,7 @@ var contributors = [
   { username: 'axelrammon', message: 'Happy Hacktoberfest 2020 and drink coffee!!' },
   { username: 'Tejasvi2', message: 'Happy Coding!!! #Hacktoberfest2020' },
   { username: 'ElekN', message: 'Happy Hacktoberfest :DDDDDD' },
+  { username: 'drbalar', message: 'Happy Hacktoberfest 2020 !' },
   { username: 'LuanaBoune', message: 'Have a quack Hacktober' },
   { username: 'LumaLiana', message: 'Quack Quack' },
 ];
