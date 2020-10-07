efe669929d2b26bd84d6b0ea0e045612f2883f9f
Aditee Singh <singhaditee33@gmail.com>
2020-10-07 17:55:58 +0530

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -131,4 +131,5 @@ var contributors = [
   { username: 'drbalar', message: 'Happy Hacktoberfest 2020 !' },
   { username: 'LuanaBoune', message: 'Have a quack Hacktober' },
   { username: 'LumaLiana', message: 'Quack Quack' },
+  {username:  'aditee-singh', message:'Happy Quacktober Fest :P'}
 ];
