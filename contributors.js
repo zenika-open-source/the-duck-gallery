f0f130ecb12b7142db9a2cb4de68d06d24b6874d
Thomas Betous <tbetous@users.noreply.github.com>
2020-10-08 17:49:15 +0200

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -135,6 +135,7 @@ var contributors = [
   { username: 'aditee-singh', message:'Happy Quacktober Fest :P'},
   { username: 'Taranys', message: 'No matter the bugs! No matter the issues! No matter what... K33P COD1NG 🍻' },
   { username: 'pyaillet', message:'\\_o< qwack qwack ! >o_/'},
+  { username: 'tbetous', message:'Je collectionne des canards vivants ! ❤️🦆🎸🤘🎼🎵' },
   { username: 'Pratiksha0100', message: 'Stop acting so small. You are the universe in ecstatic motion!' },
   { username: 'cycy-realbeats', message:'OMG ! First PR EVER... Thank u ❤️'},
 ];
