6f6801a044cfe5b5902b0217a4a50ddab5250c0d
cycy-realbeats <57004116+cycy-realbeats@users.noreply.github.com>
2020-10-08 17:46:58 +0200

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -135,4 +135,5 @@ var contributors = [
   { username: 'aditee-singh', message:'Happy Quacktober Fest :P'},
   { username: 'Taranys', message: 'No matter the bugs! No matter the issues! No matter what... K33P COD1NG 🍻' },
   { username: 'pyaillet', message:'\\_o< qwack qwack ! >o_/'},
+  { username: 'cycy-realbeats', message:'OMG ! First PR EVER... Thank u ❤️'},
 ];
