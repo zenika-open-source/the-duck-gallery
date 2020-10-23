4ff4c9aa2b1da4c72d222071e0476100dc1a26eb
fabiotph <fabiotperez@gmail.com>
2020-10-23 06:19:56 -0300

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -147,6 +147,7 @@ var contributors = [
   { username: 'srevereault', message: '🦆 Le gentil canaaaaaaaaaaaaaaaard !' },
   { username: 'y0an', message: '🦆 H4PPY H4CK7083rF357 2020!! ❤️ Fr0M r3NN35 ' },
   { username: 'Riduidel', message:'Open-source will conquer the 🌍'},
+  { username: 'fabiotph', message:'Happy Hacktober Fest 2020 💚'},
   { username: 'vinijabes', message:'We are borg!'},
   { username: 'subedibimal', message: 'Happy Hacktoberfest 2020 :) !!' },
   { username: 'GiuliaCampos', message: 'Don’t blink 😉' },
