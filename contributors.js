f548ad6984772c1794c511008d6c389953d13056
David Ono <55718959+D-Ono@users.noreply.github.com>
2020-10-23 06:34:16 -0300

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -148,6 +148,7 @@ var contributors = [
   { username: 'srevereault', message: '🦆 Le gentil canaaaaaaaaaaaaaaaard !' },
   { username: 'y0an', message: '🦆 H4PPY H4CK7083rF357 2020!! ❤️ Fr0M r3NN35 ' },
   { username: 'Riduidel', message:'Open-source will conquer the 🌍'},
+  { username: 'D-Ono', message:'Believe in Yourself and Happy Hacktober Fest.' },
   { username: 'mfrance14', message:' HacktoberFest ;) '},
   { username: 'DayaToledo', message:'Hacktober Fest 2020!'},
   { username: 'lucasgstumpf', message:'Feliz Hacktober Fest!!! Thanks you! '},
