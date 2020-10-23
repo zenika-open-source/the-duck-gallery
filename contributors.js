a41e2df6f1dcbfce68a074c72a9663ec0e998360
Thiago Valle <46169077+zthiagovalle@users.noreply.github.com>
2020-10-23 05:47:58 -0300

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -147,6 +147,7 @@ var contributors = [
   { username: 'srevereault', message: '🦆 Le gentil canaaaaaaaaaaaaaaaard !' },
   { username: 'y0an', message: '🦆 H4PPY H4CK7083rF357 2020!! ❤️ Fr0M r3NN35 ' },
   { username: 'Riduidel', message:'Open-source will conquer the 🌍'},
+  { username: 'zthiagovalle', message:'Hurulll, Hello World!'},
   { username: 'gabrielsmantovani', message:'Happy hacktoberfest guys!'},
   { username: 'FOX6683', message:'HACKTOBEER-FEST!!!'},
   { username: 'karenalmeida18', message: 'my duck drinking coffee'},
