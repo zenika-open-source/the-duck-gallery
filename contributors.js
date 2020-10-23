1b46ad67dae9566c8b7491dd385047c5a1fa43fa
Edouard Penin <e.peninb@gmail.com>
2020-10-23 13:37:13 +0200

    chore(contributors): Add isthatcentered

--- contributors.js
+++ contributors.js
@@ -201,5 +201,6 @@ var contributors = [
   { username: 'TheBossTH', message:'Happy Hacktober Fest 2020 ٩(^ᴗ^)۶'},
   { username: 'Chawklate', message:'Happy Hacktober Fest 2020 ♡ ～(\'▽^人)'},
   { username: 'tintinmaster', message:'There are some who call me … Tim' },
+  { username: 'isthatcentered', message:'👨‍🍳🦆🤦‍' }
 ];
 
