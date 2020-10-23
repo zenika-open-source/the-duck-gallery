a1c4c68255368664bf6eb8db548d7eeea1cb9571
Vincent Gilles <16140019+Rude-Monkey@users.noreply.github.com>
2020-10-23 11:46:27 +0200

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -149,6 +149,7 @@ var contributors = [
   { username: 'srevereault', message: '🦆 Le gentil canaaaaaaaaaaaaaaaard !' },
   { username: 'y0an', message: '🦆 H4PPY H4CK7083rF357 2020!! ❤️ Fr0M r3NN35 ' },
   { username: 'Riduidel', message:'Open-source will conquer the 🌍'},
+  { username: 'Rude-Monkey', message:'Fier De Présenter: Zenikanard-Licorne (Et Son Café) !!!'},
   { username: 'devmperso01', message: 'nice game for evry body' },
   { username: 'abishek-kafle', message:'Happy Hacktober fest 2020'},
   { username: 'saguins', message: 'Coffee is my life. 🦆☕' },
