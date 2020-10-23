08c7bfc0e7381ac71dfa414748bc421a0ee0517a
devmperso01 <73281881+devmperso01@users.noreply.github.com>
2020-10-23 11:44:38 +0200

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -149,6 +149,7 @@ var contributors = [
   { username: 'srevereault', message: '🦆 Le gentil canaaaaaaaaaaaaaaaard !' },
   { username: 'y0an', message: '🦆 H4PPY H4CK7083rF357 2020!! ❤️ Fr0M r3NN35 ' },
   { username: 'Riduidel', message:'Open-source will conquer the 🌍'},
+  { username: 'devmperso01', message: 'nice game for evry body' },
   { username: 'abishek-kafle', message:'Happy Hacktober fest 2020'},
   { username: 'saguins', message: 'Coffee is my life. 🦆☕' },
   { username: 'etakao', message:'Stay safe, stay coding at home!'},
