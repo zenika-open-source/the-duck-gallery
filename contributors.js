a36dc07d3fcb0e966390f7ad2757889f16685c88
Lucas G. Ferreira <lucas.g.ferreira@outlook.com.br>
2020-10-23 06:35:30 -0300

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -148,6 +148,7 @@ var contributors = [
   { username: 'srevereault', message: '🦆 Le gentil canaaaaaaaaaaaaaaaard !' },
   { username: 'y0an', message: '🦆 H4PPY H4CK7083rF357 2020!! ❤️ Fr0M r3NN35 ' },
   { username: 'Riduidel', message:'Open-source will conquer the 🌍'},
+  { username: 'MinorLess', message:'Happy HacktoberFest 2020!!!'},
   { username: 'D-Ono', message:'Believe in Yourself and Happy Hacktober Fest.' },
   { username: 'mfrance14', message:' HacktoberFest ;) '},
   { username: 'DayaToledo', message:'Hacktober Fest 2020!'},
