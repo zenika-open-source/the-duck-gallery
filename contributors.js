169c066d6dd93e886e542cea1511090058d6fa1b
MahmoudSfn <35277201+MahmoudSfn@users.noreply.github.com>
2020-10-23 11:47:07 +0200

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -10,6 +10,7 @@
  */
 /* eslint no-unused-vars: "off" */
 var contributors = [
+  { username: 'MahmoudSfn', message: 'NaN' },
   { username: 'GuiCT49', message: 'Happy Hacktoberfest 2020 ;) é os guri :)' },
   { username: 'GabrielaSinastre', message: 'Happy HacktoberFest 2020!!! SOFRO ;)' },
   { username: 'MaykonDouglas21', message: 'Yay, its my first HacktoberFest, hope yall enjoy it !! Go open source' },
