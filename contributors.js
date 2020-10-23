2375ea426af37a192da87ea0f9f027c182391897
Gabriela Sinastre <55140599+GabrielaSinastre@users.noreply.github.com>
2020-10-23 06:21:26 -0300

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -10,6 +10,7 @@
  */
 /* eslint no-unused-vars: "off" */
 var contributors = [
+  { username: 'GabrielaSinastre', message: 'Happy HacktoberFest 2020!!! SOFRO ;)' },
   { username: 'MaykonDouglas21', message: 'Yay, its my first HacktoberFest, hope yall enjoy it !! Go open source' },
   { username: 'Amagash', message: 'Have a great Hacktoberfest 2020! Happy coding!' },
   { username: 'PrakalpTiwari137', message: 'Happy HacktoberFest 2020!!! Lets open source ;)' },
