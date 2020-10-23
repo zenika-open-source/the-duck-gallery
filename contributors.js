3041e6e857396662e945f5db056e23205a34478e
Erick Takao K. Yoshike <38158461+etakao@users.noreply.github.com>
2020-10-23 06:40:53 -0300

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -148,6 +148,7 @@ var contributors = [
   { username: 'srevereault', message: '🦆 Le gentil canaaaaaaaaaaaaaaaard !' },
   { username: 'y0an', message: '🦆 H4PPY H4CK7083rF357 2020!! ❤️ Fr0M r3NN35 ' },
   { username: 'Riduidel', message:'Open-source will conquer the 🌍'},
+  { username: 'etakao', message:'Stay safe, stay coding at home!'},
   { username: 'Zenigata', message:'Just Duck It 🦆'},
   { username: 'GitJooJ', message: 'Ducks are cool.' },
   { username: 'Giovannacm', message:'Happy Hacktoberfest 2020! Quack Quack!'},
