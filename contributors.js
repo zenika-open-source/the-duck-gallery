06100d0c2f9023364784892e9811be20cf7fd62d
AnaelleZenika <31992503+AnaelleZenika@users.noreply.github.com>
2020-10-27 15:41:07 +0100

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -203,6 +203,7 @@ var contributors = [
   { username: 'tintinmaster', message:'There are some who call me … Tim' },
   { username: 'yodur2potassium', message:'My new look as an office clerk! Happy Hacktoberfest2020' },
   { username: 'isthatcentered', message:'👨‍🍳🦆🤦‍' },
+  { username: 'anaellezenika', message:'Happy DuckBerfest' },
   { username: 'patou', message:'My duck for the hacktoberfest' },
 ];
 
