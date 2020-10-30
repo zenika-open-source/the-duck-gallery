0a79871de1854ca8611da8326914e4397a422758
Madd-Scientist <skartikey7@gmail.com>
2020-10-30 20:48:31 +0530

    added duck

--- contributors.js
+++ contributors.js
@@ -210,5 +210,6 @@ var contributors = [
   { username: 'gfeun', message:'Never too late, happy hacktoberfest !' },
   { username: 'anaellezenika', message:'Happy DuckBerfest' },
   { username: 'patou', message:'My duck for the hacktoberfest' },
+  { username: 'madd-scientist', message:'Ahoy!' },
 ];
 
