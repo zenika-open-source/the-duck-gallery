440015c4e6d292a974ed121a10e5bcfd3bddb519
Estelle Landry <estelle.landry@pix.fr>
2020-11-01 16:20:19 +0100

    :sparkles: Add duck

--- contributors.js
+++ contributors.js
@@ -11,6 +11,7 @@
 /* eslint no-unused-vars: "off" */
 var contributors = [
   { username: 'MahmoudSfn', message: 'NaN' },
+  { username: 'estellelandry', message: 'In duck-unicorn we trust 🦄' },
   { username: 'GuiCT49', message: 'Happy Hacktoberfest 2020 ;) é os guri :)' },
   { username: 'GabrielaSinastre', message: 'Happy HacktoberFest 2020!!! SOFRO ;)' },
   { username: 'MaykonDouglas21', message: 'Yay, its my first HacktoberFest, hope yall enjoy it !! Go open source' },
