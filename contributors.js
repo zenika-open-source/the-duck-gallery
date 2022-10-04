57327e623e0c60e89f0d94543ee1b9eeb0a34f26
Sanvi Sharma <91133247+sanvisharma003@users.noreply.github.com>
2022-10-04 02:29:54 -0700

    added the duck image (#289)
    added duck image

--- contributors.js
+++ contributors.js
@@ -10,5 +10,6 @@
  */
 /* eslint no-unused-vars: "off" */
 var contributors = [
-    { username: 'CaioSouzalimaa', message: 'OOOOSSSS 🙏!' }
+    { username: 'CaioSouzalimaa', message: 'OOOOSSSS 🙏!' },
+    { username: 'sanvisharma003', message: 'happy hacktoberfest 2022' }
 ];
