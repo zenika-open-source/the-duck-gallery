939a853c4f08f707b70f8c6af2559943137600ac
BT <bntruong44@gmail.com>
2023-10-03 20:30:50 +0200

    je veux etre avant lionel

--- contributors.js
+++ contributors.js
@@ -33,4 +33,6 @@ var contributors = [
     { username: 'lukastorresu', message: 'Happy Hacktoberfest yall :)' },
     { username: 'Igorjr19', message: 'Happy Hacktoberfest 2022!' },
     { username: 'aryan-parmar', message: 'I love Cats, zenitsu fan'}
+    { username: 'Puncky Ducky', message: 'Drink beer & love Duck.'}
+
 ];
