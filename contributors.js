0d47ea9e69a90d7cc4dcddddfb4d86f829850f00
diana4944 <146870492+diana4944@users.noreply.github.com>
2023-10-03 19:52:03 +0200

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -9,8 +9,9 @@
  * The message must be short and respect our code of conduct.
  */
 /* eslint no-unused-vars: "off" */
-var contributors = [
-    { username: 'jeanphibaconnais', message: '🎃 Happy Hacktoberfest!' },
+var contributors = [  { username: 'diana', message: 'Oupps,first time for me' },
+
+  { username: 'jeanphibaconnais', message: '🎃 Happy Hacktoberfest!' },
     { username: 'jeanlamarre16', message: 'hacktoberfest with zenika' },
     { username: 'CaioSouzalimaa', message: 'OOOOSSSS 🙏!' },
     { username: 'sanvisharma003', message: 'happy hacktoberfest 2022' },
