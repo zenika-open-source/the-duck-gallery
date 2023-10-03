f05d2be9d80f03ff979319fcaa2999f9e5e11061
HeleneAmouzou <120368594+HeleneAmouzou@users.noreply.github.com>
2023-10-03 19:57:32 +0200

    Add my duck (#321)
    * Add duck image
    
    * Update contributors.js

--- contributors.js
+++ contributors.js
@@ -32,5 +32,6 @@ var contributors = [
     { username: 'dhruv2003', message: 'I Love Gaming' },
     { username: 'lukastorresu', message: 'Happy Hacktoberfest yall :)' },
     { username: 'Igorjr19', message: 'Happy Hacktoberfest 2022!' },
-    { username: 'aryan-parmar', message: 'I love Cats, zenitsu fan'}
+    { username: 'aryan-parmar', message: 'I love Cats, zenitsu fan'},
+    { username: 'HeleneAmouzou', message: 'Happy Hacktoberfest 2023 ! :)' }
 ];
