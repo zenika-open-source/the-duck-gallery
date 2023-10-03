a14d4b31f1f48fb83a8c393089d551b296397f79
Gwen <123936878+GwenNicolas@users.noreply.github.com>
2023-10-03 19:54:19 +0200

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -33,4 +33,5 @@ var contributors = [
     { username: 'lukastorresu', message: 'Happy Hacktoberfest yall :)' },
     { username: 'Igorjr19', message: 'Happy Hacktoberfest 2022!' },
     { username: 'aryan-parmar', message: 'I love Cats, zenitsu fan'}
+    { username: 'GwenNicolas', message: 'My first Hacktoberfest!!! <3'}
 ];
