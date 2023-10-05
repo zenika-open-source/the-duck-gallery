c0d343b592f0a91871a81f241b0c3f88a8c613f7
Mradula Mittal <mradulammittal@gmail.com>
2023-10-05 14:57:17 +0530

    Updated contributors.js
    Added username to the contributors file.

--- contributors.js
+++ contributors.js
@@ -18,5 +18,6 @@ var contributors = [
     { username: 'Reayz', message: 'Hello World!!!' },
     { username: 'gouz', message: 'Hacktoberfest 2023! Happy OpenSource Month!' },
     { username: 'ankushbhardwaj408', message: 'First contribution!!' },
-    { username: 'reverb001', message: 'Hacky Sack Oct 2023!' }
+    { username: 'reverb001', message: 'Hacky Sack Oct 2023!' },
+    { username: 'Mradula-Mittal', message: 'It is fun! Happy Hacktoberfest !' }
 ];
