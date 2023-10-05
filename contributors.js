37d2a5de8d9a7df7bc0501e7b751fee4186072f6
unknown <vinjamurimanojna@gmail.com>
2023-10-05 22:40:30 +0530

    added duck

--- contributors.js
+++ contributors.js
@@ -19,5 +19,6 @@ var contributors = [
     { username: 'gouz', message: 'Hacktoberfest 2023! Happy OpenSource Month!' },
     { username: 'ankushbhardwaj408', message: 'First contribution!!' },
     { username: 'd-coder111', message: 'Happy Learning ✌!!' },
-    { username: 'reverb001', message: 'Hacky Sack Oct 2023!' }
+    { username: 'reverb001', message: 'Hacky Sack Oct 2023!' },
+    { username: 'manojna191', message: 'Believe in yourself, you will do it ⭐' },
 ];
