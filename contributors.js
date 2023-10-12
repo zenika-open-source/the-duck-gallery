82537dc29ea37856fc9da9eedadc46d34fcc0b82
brijitha.av <brijitha.av@m.ecsfin.com>
2023-10-12 21:22:58 +0530

    feat(add-duck): add a duck

--- contributors.js
+++ contributors.js
@@ -9,19 +9,20 @@
  * The message must be short and respect our code of conduct.
  */
 /* eslint no-unused-vars: "off" */
-var contributors = [ 
-    { username: 'HeleneAmouzou', message: 'Happy Hacktoberfest 2023 ! :)' },
-    { username: 'Zohera44', message:'Hello'},
-    { username: 'GwenNicolas', message: 'My first Hacktoberfest!!! <3'},
-    { username: 'Azertoth', message: 'Les canards vaincront !!' },
-    { username: 'diana4944', message: 'Oupps,first time for me' },
-    { username: 'Reayz', message: 'Hello World!!!' },
-    { username: 'gouz', message: 'Hacktoberfest 2023! Happy OpenSource Month!' },
-    { username: 'ankushbhardwaj408', message: 'First contribution!!' },
-    { username: 'd-coder111', message: 'Happy Learning ✌!!' },
-    { username: 'ikunal-04', message: 'Happy Hacktoberfest! :)' },
-    { username: 'reverb001', message: 'Hacky Sack Oct 2023!' },
-    { username: 'Mradula-Mittal', message: 'It is fun! Happy Hacktoberfest !' },
-    { username: 'kahenya-anita', message: 'Open sauced!' },
-    { username: 'manojna191', message: 'Believe in yourself, you will do it ⭐' }
+var contributors = [
+  { username: "HeleneAmouzou", message: "Happy Hacktoberfest 2023 ! :)" },
+  { username: "Zohera44", message: "Hello" },
+  { username: "GwenNicolas", message: "My first Hacktoberfest!!! <3" },
+  { username: "Azertoth", message: "Les canards vaincront !!" },
+  { username: "diana4944", message: "Oupps,first time for me" },
+  { username: "Reayz", message: "Hello World!!!" },
+  { username: "gouz", message: "Hacktoberfest 2023! Happy OpenSource Month!" },
+  { username: "ankushbhardwaj408", message: "First contribution!!" },
+  { username: "d-coder111", message: "Happy Learning ✌!!" },
+  { username: "ikunal-04", message: "Happy Hacktoberfest! :)" },
+  { username: "reverb001", message: "Hacky Sack Oct 2023!" },
+  { username: "Mradula-Mittal", message: "It is fun! Happy Hacktoberfest !" },
+  { username: "kahenya-anita", message: "Open sauced!" },
+  { username: "manojna191", message: "Believe in yourself, you will do it ⭐" },
+  { username: "Brij247", message: "Happy coding" },
 ];
