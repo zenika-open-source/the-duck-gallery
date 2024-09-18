7a0a4d818a331a9d631a814b02b86384104c897d
jeanphi.baconnais <jeanphilippe.baconnais@gmail.com>
2024-09-18 20:38:44 +0000

    :sparkles: Reset ducks

--- contributors.js
+++ contributors.js
@@ -10,22 +10,4 @@
  */
 /* eslint no-unused-vars: "off" */
 var contributors = [ 
-    { username: 'HeleneAmouzou', message: 'Happy Hacktoberfest 2023 ! :)' },
-    { username: 'Zohera44', message:'Hello'},
-    { username: 'GwenNicolas', message: 'My first Hacktoberfest!!! <3'},
-    { username: 'Azertoth', message: 'Les canards vaincront !!' },
-    { username: 'diana4944', message: 'Oupps,first time for me' },
-    { username: 'Reayz', message: 'Hello World!!!' },
-    { username: 'gouz', message: 'Hacktoberfest 2023! Happy OpenSource Month!' },
-    { username: 'ankushbhardwaj408', message: 'First contribution!!' },
-    { username: 'DevAgrawal04', message: 'git push --force' },
-    { username: 'd-coder111', message: 'Happy Learning ✌!!' },
-    { username: 'ikunal-04', message: 'Happy Hacktoberfest! :)' },
-    { username: 'reverb001', message: 'Hacky Sack Oct 2023!' },
-    { username: 'Mradula-Mittal', message: 'It is fun! Happy Hacktoberfest !' },
-    { username: 'kahenya-anita', message: 'Open sauced!' },
-    { username: 'manojna191', message: 'Believe in yourself, you will do it ⭐' },
-    { username: 'jeanphi-baconnais', message: 'Happy Hacktoberfest 2023 🎉' },
-    { username: 'Brij247', message: 'Happy Coding' },
-    { username: 'mandeep-s1ngh', message: 'Happy Ducktoberfest!' }
 ];
