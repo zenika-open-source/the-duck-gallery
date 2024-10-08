5fcf5847bc3e0d1395f4f3314ffb016b6b1adf25
Shaurya Verma <sagittariusshaurya5@gmail.com>
2024-10-08 08:31:01 +0530

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -22,6 +22,6 @@ const contributors = [
   { username: 'JayShukla8', message: 'Autumn\'s here!' },
   { username: 'SameerKhans13', message: 'Happy Hacktoberfest-24 to all!' },
   { username: 'rishi-harti768', message: 'Happy Hacktoberfest guys! cheers'},
-  { username: 'Hathaipach', message: 'Happy Hacktoberfest 2024'}
+  { username: 'Hathaipach', message: 'Happy Hacktoberfest 2024'},
   { username: 'Astro-Dude', message: 'print('Happy Hacktoberfest-24 to all!'); if (prMerged): print('Happy') else: print('please, accept')'}
 ];
