b64f8ffc67fa3ae206ce903aa1dacde6a2d1a047
Kartikey060105 <kartikey060105@gmail.com>
2024-10-16 00:32:59 +0530

    Add my Duck (#391)
    * Add duck image.
    ---------
    
    Co-authored-by: Jean-Phi Baconnais <jeanphilippe.baconnais@gmail.com>

--- contributors.js
+++ contributors.js
@@ -40,5 +40,6 @@ const contributors = [
   { username: 'Dhruv-Davda', message:'Good Wishes Everyone' },
   { username: 'Piyush-Goenka', message:'October is hacked' },
   { username: 'MeuHubPython', message: 'My duck is the best! Happy Hacktoberfest!' },
+  { username: 'Kartikey060105', message: 'What the duck 𓅭' },
   { username: 'xnisha-verma', message: 'Cheers to Hacktoberfest 2024 :)' }
 ];
