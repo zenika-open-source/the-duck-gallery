d5084d757014d7234b244f8da0932cdae3b3b665
its-wasp <140952737+its-wasp@users.noreply.github.com>
2024-10-28 17:47:42 +0530

    Added my Duck (#413)
    * Add files via upload

--- contributors.js
+++ contributors.js
@@ -59,5 +59,6 @@ const contributors = [
   { username: 'Sidd-hant13', message: 'hip hip hurray, hacktoberfest 2024!' },
   { username: 'Shreyhac', message: 'happy coding' },
   { username: 'srujangowda07', message: 'First Request' },
-  { username: 'sohailshk', message: 'First pr hopefully for hacktoberfest2024' }
+  { username: 'sohailshk', message: 'First pr hopefully for hacktoberfest2024' },
+  { username: 'its-wasp', message: 'I\'m the hecker boi'}
 ];
