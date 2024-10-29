b8edbf7cd708af9e5d2335297bafcdaae90c468b
Karthikeya914 <karthikeya.chakravaram@gmail.com>
2024-10-29 15:02:39 +0530

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -64,4 +64,5 @@ const contributors = [
   { username: 'sohailshk', message: 'First pr hopefully for hacktoberfest2024' },
   { username: 'snehachandra1108', message: 'First Hacktober' },
   { username: 'its-wasp', message: 'I\'m the hecker boi'}
+  { username: 'Karthikeya914', message: 'Duck Quack' },
 ];
