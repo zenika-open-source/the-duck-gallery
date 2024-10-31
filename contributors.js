1949c2e92e5e2a2f6045e1bae9351623aaee46ef
Ravijot Sinha <156286864+RJSIN147@users.noreply.github.com>
2024-10-31 19:12:03 +0530

    Update contributors.js

--- contributors.js
+++ contributors.js
@@ -75,4 +75,5 @@ const contributors = [
   { username: 'Yeashu', message: 'Hello Hacktoberfest. Here my journey begin'},
   { username: 'sanaa-duhh', message: 'starting my hacktoberfest journey with this one'},
   { username: 'SayedZahur786', message: 'Hello Zenika, Thanks for this Pr'}
+  { username: 'RJSIN147', message: 'Thanks for this repo.'}
 ];
